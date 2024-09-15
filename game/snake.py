0c6b011800101fbfb27c317375437ffb03b119f8
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2024-09-15 15:50:56 +0200

    Updated game so it's ready for AI and not user

--- game/snake.py
+++ game/snake.py
@@ -1,11 +1,23 @@
 import pygame
 import random
+import numpy as np
 from enum import Enum
 from collections import namedtuple # This is a tuple with named fields
 # Tuple ELI5: It's like a list, but you can't change the values inside it.
 
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
+
+# Reset function for agent to be able to reset the game and start again
+
+# Reward function to give the agent a reward for eating the food and a penalty for dying
+
+# Play(action) function to play the game based on the action the agent takes
+
+# Game iteration
+
+# is_collision
+
 
 
 class Direction(Enum): 
@@ -28,7 +40,7 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 
-class SnakeGame:
+class SnakeGameAI:
     def __init__(self, w=640, h=480):
         self.w = w
         self.h = h
@@ -38,9 +50,12 @@ class SnakeGame:
         self.display = pygame.display.set_mode((self.w, self.h)) # width, height
         pygame.display.set_caption('Snake Game')
         self.clock = pygame.time.Clock() # To control speed of game
+        self.reset()
+        
         
 
 
+    def reset(self):
         # init game state:
         self.direction = Direction.RIGHT
 
@@ -54,6 +69,7 @@ class SnakeGame:
 
         # Place food
         self._place_food()
+        self.frame_iteration = 0
 
 
     def _place_food(self): # This is a private method or a helper method
@@ -66,36 +82,31 @@ class SnakeGame:
 
         
 
-    def play_step(self):
+    def play_step(self, action):
+        self.frame_iteration += 1 # To keep track of how many frames have passed
         # 1. User Input
         for event in pygame.event.get() : # Get's the user input
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            # Movement
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.direction = Direction.UP
-                elif event.key == pygame.K_DOWN:
-                    self.direction = Direction.DOWN
-                elif event.key == pygame.K_LEFT:
-                    self.direction = Direction.LEFT
-                elif event.key == pygame.K_RIGHT: # Not Else because then any key would be considered right
-                    self.direction = Direction.RIGHT
+
 
         # 2. Move Snake
-        self._move(self.direction) # Update the head
+        self._move(action) # Update the head
         self.snake.insert(0, self.head) # Inserting the head of the snake at the beginning of the list
 
         # 3. Check if Game Over
+        reward = 0
         game_over = False
-        if self._is_collision():
+        if self._is_collision() or self.frame_iteration > 100*len(self.snake): # So that the game ends if the AI doesn't do anything
             game_over = True
-            return game_over, self.score
+            reward = -10
+            return reward, game_over, self.score
 
         # 4. Place new food if we hit food, or just move snake
         if self.head == self.food: # If the head of the snake is at the same position as the food
             self.score += 1
+            reward = 10
             self._place_food() # Place new food at a random location
         else: 
             self.snake.pop() # Remove the last element of the snake so that it is not growing infinitely
@@ -105,7 +116,7 @@ class SnakeGame:
         self.clock.tick(SPEED) # SPEED frames per second
 
         # 6. Return game over and score
-        return game_over, self.score
+        return reward, game_over, self.score
 
     def _update_ui(self):
         self.display.fill(BLACK) # First fill screen before drawing anything else
@@ -123,46 +134,46 @@ class SnakeGame:
         pygame.display.flip() # Update the display
 
 
-    def _move(self, direction):
-        # Extract the values of the head
+    def _move(self, action):
+        # [straight, right, left] in the format [0, 1, 0] = right
+
+        clockwise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clockwise.index(self.direction) # Get the index of the current direction
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_direction = clockwise[idx] # No change in direction
+        elif np.array_equal(action, [0, 1, 0]): # right turn = right -> down -> left -> up
+            next_idx = (idx + 1) % 4 # To make sure that the index doesn't go out of bounds and stays within 0, 1, 2, 3
+            new_direction = clockwise[(idx)] # Turn right
+        else: # [0, 0, 1] # left turn = right -> up -> left -> down
+            next_idx = (idx - 1) % 4 # Same as above but for turning left
+            new_direction = clockwise[next_idx]
+
+        self.direction = new_direction
+
+
         x = self.head.x 
         y = self.head.y
 
-        if direction == Direction.RIGHT:
+        if self.direction == Direction.RIGHT:
             x += BLOCKSIZE
-        elif direction == Direction.LEFT:
+        elif self.direction == Direction.LEFT:
             x -= BLOCKSIZE
-        elif direction == Direction.DOWN:
+        elif self.direction == Direction.DOWN:
             y += BLOCKSIZE
-        elif direction == Direction.UP:
+        elif self.direction == Direction.UP:
             y -= BLOCKSIZE
 
         self.head = Point(x, y)
 
-    def _is_collision(self):
+    def _is_collision(self, pt=None): # pt is the point we want to check for collision
         # If it hits the border
-        if self.head.x > self.w - BLOCKSIZE or self.head.x < 0 or self.head.y > self.h - BLOCKSIZE or self.head.y < 0:
+        if pt is None:
+            pt = self.head
+        if pt.x > self.w - BLOCKSIZE or pt.x < 0 or pt.y > self.h - BLOCKSIZE or pt.y < 0:
             return True
         # If it hits itself
-        if self.head in self.snake[1:] : # We don't want to check the head because it's the first element
+        if pt in self.snake[1:] : # We don't want to check the head because it's the first element
             return True
         
         return False
-
-# Main Function
-if __name__ == '__main__':
-    game = SnakeGame()
-
-    # Game Loop
-
-    while True:
-        game_over, score = game.play_step()
-
-        # If game over
-        if game_over == True:
-            break
-        
-    print('Final Score', score)
-        
-
-    pygame.quit()
